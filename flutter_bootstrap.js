3132e9fe55d6f6004ec975d6db07338b94bce553
Albert Einstein <javastraat@hotmail.com>
2024-12-26 16:35:19 +0100

    DMR User Database (1.0.20241226) (138)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3457428868"
+    serviceWorkerVersion: "3488482408"
   }
 });
